f02351c79ff0a0337a126c3f19a9c2ef88260c5c
camismacho <camismacho@yahoo.com>
2019-08-29 22:25:34 -0600

    Update with hw0wc.c

--- hw0wc.c
+++ hw0wc.c
@@ -9,18 +9,30 @@
 
 	int main(int argc, char *argv[]){
 
-		int i;
 		FILE *fp;
 
-		char line[300], *s;
+		//char line[300];
 
 		//if first arg is the filename, open it
 		fp = fopen(argv[1], "r");
 		if (fp){
 
-			printf("fgets %p\n", fgets(line, sizeof(line), fp));
+			int words = 0;
+					int lines = 0;
+					char letters[300];
 
-			printf("line  = (%s) size = %lu length = %lu\n", line, sizeof(line), strlen(line));
+					fgets(letters, sizeof(letters), fp);
+
+					char* token = strtok(letters, " \n\t\r");
+
+					while (token != NULL){
+						printf("\n%s\n", token);
+						words++;
+						char* token = strtok(NULL, " \n\t\r");
+					}
+
+
+					printf("line  = (%s) size = %lu length = %lu\n", letters, sizeof(letters), strlen(letters));
 
 			fclose(fp);
 		}
@@ -28,18 +40,32 @@
 		//if there is no command line arg, then use stdin
 		fp = stdin;
 
-		printf("fgets %p\n", fgets(line, sizeof(line), fp));
+		//processLine(fp);
 
-		printf("line  = (%s) size = %lu length = %lu\n", line, sizeof(line), strlen(line));
 
+	return 0;
+
+	}
+
+	/*int processLine(FILE fileName){
+
+		int words = 0;
+		int lines = 0;
+		char letters[300];
+
+		fgets(letters, sizeof(letters), fileName);
+
+		char* token = strtok(letters, " \n\t\r");
+
+		while (token != NULL){
+			printf("\n%s\n", token);
+			words++;
+			char* token = strtok(NULL, " \n\t\r");
+		}
+
+
+		printf("line  = (%s) size = %lu length = %lu\n", letters, sizeof(letters), strlen(letters));
 
 		return 0;
+	}*/
 
-
-	}
-
-	int processLine(char inputLine){
-
-
-	}
-}
