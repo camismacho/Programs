92be1cb72b1e03056386ebf908c97e4298f8b27d
camismacho <camismacho@yahoo.com>
2019-09-05 09:50:52 -0600

    Progress on 370 Lab 2

--- CS 370/Lab 2/hw2rdp.c
+++ CS 370/Lab 2/hw2rdp.c
@@ -7,10 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//global variable
 char lookahead;
+//fucntion prototypes
 void match(char terminal);
 void nontermS();
-void nontermA();
+int nontermA(int count);
 void nontermB();
 
 int main (int argc, char **argv) {
@@ -19,45 +21,58 @@ int main (int argc, char **argv) {
 	do {
 		nontermS();
 	} while (lookahead != EOF);
-}
+}//end main
 
 void match(char terminal) {
+	//get next char if terminal is a match to lookahead
 	if (lookahead == terminal) {
 		lookahead = getchar();
 	}
+	//print a syntax error if no match
 	else {
 		printf ("Syntax error! (%c)\n", lookahead);
 		exit(0);
 	}
-}
+}//end match
 
 void nontermS() {
-	nontermA();
+	//counters for # of A's, B's, and postiion for syntax error
+	int countA = 0;
+	int countB = 0;
+	int position = 0;
+	//begin searching for grammer
+	countA = nontermA(countA);
 	nontermB();
 	match('\n');
-	printf("Matched!\n");
-}
+	printf("Matched! # of A's: %d \n", countA);
+}//end nontermS
 
-void nontermA() {
+int nontermA(int count) {
 	switch(lookahead) {
+	//match a, increment countA and keep searching
 	case 'a':
 		match('a');
-		nontermA();
+		count++;
+		nontermA(count);
 		nontermB();
 		break;
+	//default case for empty
 	default:
 		break;
 	}
-}
+	return count;
+}//end nontermA
 
 void nontermB() {
 	switch(lookahead) {
+	// match b, inrement countB and keep searching
 	case 'b':
 		match('b');
 		nontermA();
 		nontermB();
 		break;
+	//default case for empty
 	default:
 		break;
 	}
-}
+}//end nontermB
