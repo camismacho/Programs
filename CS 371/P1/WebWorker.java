06bf69f29a84775721bed2ae1ae8458f739372fd
camismacho <camismacho@yahoo.com>
2019-09-16 12:53:41 -0600

    progress on 371 p1

--- CS 371/P1/WebWorker.java
+++ CS 371/P1/WebWorker.java
@@ -3,12 +3,12 @@
 * to receive and respond to a single HTTP request. After the constructor
 * the object executes on its "run" method, and leaves when it is done.
 *
-* One WebWorker object is only responsible for one client connection. 
+* One WebWorker object is only responsible for one client connection.
 * This code uses Java threads to parallelize the handling of clients:
 * each WebWorker runs in its own thread. This means that you can essentially
-* just think about what is happening on one client at a time, ignoring 
+* just think about what is happening on one client at a time, ignoring
 * the fact that the entirety of the webserver execution might be handling
-* other clients, too. 
+* other clients, too.
 *
 * This WebWorker class (i.e., an object of this class) is where all the
 * client interaction is done. The "run()" method is the beginning -- think
@@ -16,7 +16,7 @@
 * a row, invoking three methods in this class: it reads the incoming HTTP
 * request; it writes out an HTTP header to begin its response, and then it
 * writes out some HTML content for the response content. HTTP requests and
-* responses are just lines of text (in a very particular format). 
+* responses are just lines of text (in a very particular format).
 *
 **/
 
@@ -26,162 +26,158 @@ import java.io.*;
 import java.util.Date;
 import java.text.DateFormat;
 import java.util.TimeZone;
-import java.util.*;
 
-public class WebWorker implements Runnable
-{
+    public class WebWorker implements Runnable
+    {
 
-private Socket socket;
+    String fileName;
+    private Socket socket;
 
-/**
-* Constructor: must have a valid open socket
-**/
-public WebWorker(Socket s)
-{
-   socket = s;
-}
+    /**
+    * Constructor: must have a valid open socket
+    **/
+    public WebWorker(Socket s)
+    {
+        socket = s;
+    }
 
-/**
-* Worker thread starting point. Each worker handles just one HTTP 
-* request and then returns, which destroys the thread. This method
-* assumes that whoever created the worker created it with a valid
-* open socket object.
-**/
-public void run()
-{
-   System.err.println("Handling connection...");
-   try {
-      InputStream  is = socket.getInputStream();
-      OutputStream os = socket.getOutputStream();
-      //readHTTPRequest(is);
-      writeHTTPHeader(os,"text/html");
-      readHTTPRequest(is);
-      //writeContent(os);
-      os.flush();
-      socket.close();
-   } catch (Exception e) {
-      System.err.println("Output error: "+e);
-   }
-   System.err.println("Done handling connection.");
-   return;
-}
+    /**
+    * Worker thread starting point. Each worker handles just one HTTP
+    * request and then returns, which destroys the thread. This method
+    * assumes that whoever created the worker created it with a valid
+    * open socket object.
+    **/
+    public void run()
+    {
+        String urlAddress;
+        String contentType;  //.gif   .jpg   .png   .ico
 
-/**
-* Read the HTTP request header.
-**/
-private void readHTTPRequest(InputStream is) //probably need to modify this for P1
-{
-	try {
+        System.err.println("Handling connection...");
+        try {
+            InputStream  is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+            urlAddress = readHTTPRequest( is );
+            System.out.println( urlAddress );  //debugging
+            //finding out what type of image it is
+            /*if ( urlAddress.contains( ".gif" ) )
+                contentType = "image/gif";
+            else if ( urlAddress.contains( ".jpg" ) )
+                contentType = "image/jpeg";
+            else if ( urlAddress.contains(".png") )
+                contentType = "image/png";
+            else if ( urlAddress.contains( ".ico" ) )
+                contentType = "image/x-icon";
+            else*/
+                contentType = "text/html";
+            //send the contentType and urlAddress to writeHTTPHeader/ writeContent
+            writeHTTPHeader( os , contentType , urlAddress );  
+            writeContent( os , contentType , urlAddress );
+            os.flush();
+            socket.close();
+        } catch ( Exception e ) {
+            System.err.println( "Output error: " + e );
+        }
+        System.err.println( "Done handling connection." );
+        return;
+    }
 
-	      // Open connections to the socket
-	      BufferedReader in = new BufferedReader(new InputStreamReader(is));
-	      PrintStream out = new PrintStream(new BufferedOutputStream(socket.getOutputStream()));
+    /**
+    * Read the HTTP request header.
+    **/
+    private String readHTTPRequest( InputStream is )
+    {
+        String line;
+        BufferedReader r = new BufferedReader( new InputStreamReader( is ) );
 
-	      // Read filename from first input line "GET /filename.html ..."
-	      // or if not in this format, treat as a file not found.
-	      String s=in.readLine();
-	      System.out.println(s);  // Log the request
+        String urlAddress = "";
 
-	      // Attempt to serve the file.  Catch FileNotFoundException and
-	      // return an HTTP error "404 Not Found".  Treat invalid requests
-	      // the same way.
-	      String filename = "";
-	      StringTokenizer st = new StringTokenizer(s);
-	      try {
+        while ( true ) {
+            try {
+                while ( !r.ready() ) Thread.sleep( 1 );
+                line = r.readLine();
+                if ( line.contains( "GET " ) ) {
+                    urlAddress = line.substring( 4 ); //get rid of http from url string
+                    for( int i = 0 ; i < urlAddress.length() ; i++ ) {
+                        if ( urlAddress.charAt( i ) == ' ' )
+                            urlAddress = urlAddress.substring(0 , i );
+                    }//end for
+                }//end if
+                System.err.println( "Request line: (" + line + ")" );
+                if ( line.length() == 0 )
+                    break;
+            } catch ( Exception e ) {
+                System.err.println( "Request error: " + e );
+                break;
+            }
+        }
+        return urlAddress;
+    }
 
-	        // Parse the filename from the GET command
-	        if (st.hasMoreElements() && st.nextToken().equalsIgnoreCase("GET")
-	            && st.hasMoreElements())
-	          filename=st.nextToken();
-	        else
-	          throw new FileNotFoundException();  // Bad request
+    /**
+    * Write the HTTP header lines to the client network connection.
+    * @param os is the OutputStream object to write to
+    * @param contentType is the string MIME content type (e.g. "text/html")
+    **/
+    private void writeHTTPHeader( OutputStream os , String contentType , String urlAddress ) throws Exception
+    {
+        Date d = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        df.setTimeZone( TimeZone.getTimeZone("GMT") );
+        //if the file does not exist change the HTTP status
+        try {
+            FileReader file = new FileReader( urlAddress );
+            BufferedReader r = new BufferedReader( file );
+        } catch( FileNotFoundException e ) {
+            System.out.println( "File not found: " + urlAddress );
+            os.write( "HTTP/1.1 404 Not Found\n".getBytes() );
+        }
+        //otherwise keep the status 200 OK
+        os.write( "HTTP/1.1 200 OK\n".getBytes() );
+        os.write( "Date: ".getBytes());
+        os.write( ( df.format( d ) ).getBytes() );
+        os.write( "\n".getBytes() );
+        os.write( "Server: Joseph's very own server\n".getBytes() );
+        os.write( "Connection: close\n".getBytes() );
+        os.write( "Content-Type: ".getBytes() );
+        os.write( contentType.getBytes() );
+        os.write( "\n\n".getBytes() ); // HTTP header ends with 2 newlines
+        return;
+    }
 
-	        // Append trailing "/" with "index.html"
-	        if (filename.endsWith("/"))
-	          filename+="index.html";
+    /**
+    * Write the data content to the client network connection. This MUST
+    * be done after the HTTP header has been written out.
+    * @param os is the OutputStream object to write to
+    **/
+    private void writeContent(OutputStream os , String contentType , String urlAddress) throws Exception
+    {
+        Date d = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        df.setTimeZone(TimeZone.getTimeZone( "GMT" ));
+        String dataContent = "";
+        String urlAddressCopy = "." + urlAddress.substring( 0 , urlAddress.length( ) );
+        String date = df.format( d );
+        File in = new File( urlAddressCopy );
 
-	        // Remove leading / from filename
-	        while (filename.indexOf("/")==0)
-	          filename=filename.substring(1);
-
-	        // Replace "/" with "\" in path for PC-based servers
-	        filename=filename.replace('/', File.separator.charAt(0));
-
-	        // Check for illegal characters to prevent access to superdirectories
-	        if (filename.indexOf("..")>=0 || filename.indexOf(':')>=0
-	            || filename.indexOf('|')>=0)
-	          throw new FileNotFoundException();
-
-	        // If a directory is requested and the trailing / is missing,
-	        // send the client an HTTP request to append it.  (This is
-	        // necessary for relative links to work correctly in the client).
-	        if (new File(filename).isDirectory()) {
-	          filename=filename.replace('\\', '/');
-	          out.print("HTTP/1.0 301 Moved Permanently\r\n"+
-	            "Location: /"+filename+"/\r\n\r\n");
-	          out.close();
-	          return;
-	        }
-
-	        // Open the file (may throw FileNotFoundException)
-	        InputStream f=new FileInputStream(filename);
-
-	        // Send file contents to client, then close the connection
-	        byte[] a=new byte[4096];
-	        int n;
-	        while ((n=f.read(a))>0)
-	          out.write(a, 0, n);
-	        out.close();
-	      }
-	      catch (FileNotFoundException x) {
-	        out.println("HTTP/1.0 404 Not Found\r\n"+
-	          "Content-type: text/html\r\n\r\n"+
-	          "<html><head></head><body>"+filename+" not found</body></html>\n");
-	        out.close();
-	      }
-	    }
-	    catch (IOException x) {
-	      System.out.println(x);
-	    }
-	return;
-}
-	   
-   
-
-/**
-* Write the HTTP header lines to the client network connection.
-* @param os is the OutputStream object to write to
-* @param contentType is the string MIME content type (e.g. "text/html")
-**/
-private void writeHTTPHeader(OutputStream os, String contentType) throws Exception 
-{
-   Date d = new Date();
-   DateFormat df = DateFormat.getDateTimeInstance();
-   df.setTimeZone(TimeZone.getTimeZone("GMT"));
-   os.write("HTTP/1.1 200 OK\n".getBytes());//this also has something to do with the assignment (http status request - 404)
-   os.write("Date: ".getBytes());
-   os.write((df.format(d)).getBytes());
-   os.write("\n".getBytes());
-   os.write("Server: Jon's very own server\n".getBytes());
-   //os.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes());
-   //os.write("Content-Length: 438\n".getBytes()); 
-   os.write("Connection: close\n".getBytes());
-   os.write("Content-Type: ".getBytes());
-   os.write(contentType.getBytes());
-   os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
-   return;
-}
-
-/**
-* Write the data content to the client network connection. This MUST
-* be done after the HTTP header has been written out.
-* @param os is the OutputStream object to write to
-**/
-private void writeContent(OutputStream os) throws Exception
-{
-   os.write("<html><head></head><body>\n".getBytes());
-   os.write("<h3>I don't know what I'm doing!!</h3>\n".getBytes());
-   os.write("</body></html>\n".getBytes());
-}
+        // handel plain html text
+            try{
+                FileReader inRead = new FileReader( in );
+                BufferedReader inBuffer = new BufferedReader( inRead );
+                while( ( dataContent = inBuffer.readLine() ) != null ) {
+                    os.write( dataContent.getBytes() );
+                    os.write( "\n".getBytes());
+                    if ( dataContent.contains( "<cs371date>" ) ) {
+                        os.write( date.getBytes() );
+                    }
+                    if ( dataContent.contains( "<cs371server>" ) )
+                        os.write( "My Server's ID string\n".getBytes() );
+                }
+                
+            } catch(FileNotFoundException e) {
+                System.err.println( "File not found: " + urlAddress );
+                os.write( "<h1>Error: 404 Not found<h1>\n".getBytes() );
+            }
+        
+    }
 
 } // end class
--- CS 371/P1/webserverexample.java
+++ CS 371/P1/webserverexample.java
@@ -1,174 +1,196 @@
-/* Webserver.java -- Matt Mahoney, mmahoney@cs.fit.edu
+/**
+* Web worker: an object of this class executes in its own new thread
+* to receive and respond to a single HTTP request. After the constructor
+* the object executes on its "run" method, and leaves when it is done.
+*
+* One WebWorker object is only responsible for one client connection.
+* This code uses Java threads to parallelize the handling of clients:
+* each WebWorker runs in its own thread. This means that you can essentially
+* just think about what is happening on one client at a time, ignoring
+* the fact that the entirety of the webserver execution might be handling
+* other clients, too.
+*
+* This WebWorker class (i.e., an object of this class) is where all the
+* client interaction is done. The "run()" method is the beginning -- think
+* of it as the "main()" for a client interaction. It does three things in
+* a row, invoking three methods in this class: it reads the incoming HTTP
+* request; it writes out an HTTP header to begin its response, and then it
+* writes out some HTML content for the response content. HTTP requests and
+* responses are just lines of text (in a very particular format).
+*
+**/
 
-Webserver.java runs a simple web server.  To run, cd to the directory
-containing the .html files to be served, and run:
-
-  java Webserver >logfile
-
-To kill the server, type Ctrl-C.
-
-The server serves files only in the current directory and subdirectories.
-It does not support CGI, cookies, authentication, keepalive, etc.
-Client requests are logged to standard output.
-
-Webserver accepts HTTP GET requests that begin with:
-
-  GET /filename.html
-
-If the file exists, it responds with:
-
-  HTTP/1.0 200 OK\r\n
-  Content-Type: text/html\r\n
-  \r\n
-  (contents of filename.html)
-
-where \r\n is a carriage return and line feed.  The content type depends
-on the filename extension as follows:
-
-.html   text/html
-.htm    text/html
-.gif    image/gif
-.jpeg   image/jpeg
-.jpg    image/jpeg
-.class  application/octet-stream
-.*      text/plain  (all other extensions)
-
-The server closes the connection after returning a file.  Keepalive
-(the default in HTTP/1.1) is not implemented.
-
-If the file does not exist or the request is not understood, the server
-sends an HTTP "404 Not Found" error message back to the client.
-
-If the client requests a directory without a trailing "/",
-then the client is redirected to append it with an "HTTP 301 Moved
-Permanently" response.  If the trailing slash is present, the server
-returns the file index.html if it exists.
-
-To prevent access outside the directory tree where the server was started,
-file names containing "..", "|", or ":" are rejected.
-*/
-
-import java.util.*;
+import java.net.Socket;
+import java.lang.Runnable;
 import java.io.*;
-import java.net.*;
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.TimeZone;
 
-// A Webserver waits for clients to connect, then starts a separate
-// thread to handle the request.
-public class Webserver {
-  private static ServerSocket serverSocket;
+    public class WebWorker implements Runnable
+    {
 
-  public static void main(String[] args) throws IOException {
-    serverSocket=new ServerSocket(80);  // Start, listen on port 80
-    while (true) {
-      try {
-        Socket s=serverSocket.accept();  // Wait for a client to connect
-        new ClientHandler(s);  // Handle the client in a separate thread
-      }
-      catch (Exception x) {
-        System.out.println(x);
-      }
+    String fileName;
+    private Socket socket;
+
+    /**
+    * Constructor: must have a valid open socket
+    **/
+    public WebWorker(Socket s)
+    {
+        socket = s;
     }
-  }
-}
 
-// A ClientHandler reads an HTTP request and responds
-class ClientHandler extends Thread {
-  private Socket socket;  // The accepted socket from the Webserver
+    /**
+    * Worker thread starting point. Each worker handles just one HTTP
+    * request and then returns, which destroys the thread. This method
+    * assumes that whoever created the worker created it with a valid
+    * open socket object.
+    **/
+    public void run()
+    {
+        String urlAddress;
+        String contentType;  //.gif   .jpg   .png   .ico
 
-  // Start the thread in the constructor
-  public ClientHandler(Socket s) {
-    socket=s;
-    start();
-  }
-
-  // Read the HTTP request, respond, and close the connection
-  public void run() {
-    try {
-
-      // Open connections to the socket
-      BufferedReader in=new BufferedReader(new InputStreamReader(
-        socket.getInputStream()));
-      PrintStream out=new PrintStream(new BufferedOutputStream(
-        socket.getOutputStream()));
-
-      // Read filename from first input line "GET /filename.html ..."
-      // or if not in this format, treat as a file not found.
-      String s=in.readLine();
-      System.out.println(s);  // Log the request
-
-      // Attempt to serve the file.  Catch FileNotFoundException and
-      // return an HTTP error "404 Not Found".  Treat invalid requests
-      // the same way.
-      String filename="";
-      StringTokenizer st=new StringTokenizer(s);
-      try {
-
-        // Parse the filename from the GET command
-        if (st.hasMoreElements() && st.nextToken().equalsIgnoreCase("GET")
-            && st.hasMoreElements())
-          filename=st.nextToken();
-        else
-          throw new FileNotFoundException();  // Bad request
-
-        // Append trailing "/" with "index.html"
-        if (filename.endsWith("/"))
-          filename+="index.html";
-
-        // Remove leading / from filename
-        while (filename.indexOf("/")==0)
-          filename=filename.substring(1);
-
-        // Replace "/" with "\" in path for PC-based servers
-        filename=filename.replace('/', File.separator.charAt(0));
-
-        // Check for illegal characters to prevent access to superdirectories
-        if (filename.indexOf("..")>=0 || filename.indexOf(':')>=0
-            || filename.indexOf('|')>=0)
-          throw new FileNotFoundException();
-
-        // If a directory is requested and the trailing / is missing,
-        // send the client an HTTP request to append it.  (This is
-        // necessary for relative links to work correctly in the client).
-        if (new File(filename).isDirectory()) {
-          filename=filename.replace('\\', '/');
-          out.print("HTTP/1.0 301 Moved Permanently\r\n"+
-            "Location: /"+filename+"/\r\n\r\n");
-          out.close();
-          return;
+        System.err.println("Handling connection...");
+        try {
+            InputStream  is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+            urlAddress = readHTTPRequest( is );
+            System.out.println( urlAddress );  //debugging
+            //finding out what type of image it is
+            if ( urlAddress.contains( ".gif" ) )
+                contentType = "image/gif";
+            else if ( urlAddress.contains( ".jpg" ) )
+                contentType = "image/jpeg";
+            else if ( urlAddress.contains(".png") )
+                contentType = "image/png";
+            else if ( urlAddress.contains( ".ico" ) )
+                contentType = "image/x-icon";
+            else
+                contentType = "text/html";
+            //send the contentType and urlAddress to writeHTTPHeader/ writeContent
+            writeHTTPHeader( os , contentType , urlAddress );  
+            writeContent( os , contentType , urlAddress );
+            os.flush();
+            socket.close();
+        } catch ( Exception e ) {
+            System.err.println( "Output error: " + e );
         }
-
-        // Open the file (may throw FileNotFoundException)
-        InputStream f=new FileInputStream(filename);
-
-        // Determine the MIME type and print HTTP header
-        String mimeType="text/plain";
-        if (filename.endsWith(".html") || filename.endsWith(".htm"))
-          mimeType="text/html";
-        else if (filename.endsWith(".jpg") || filename.endsWith(".jpeg"))
-          mimeType="image/jpeg";
-        else if (filename.endsWith(".gif"))
-          mimeType="image/gif";
-        else if (filename.endsWith(".class"))
-          mimeType="application/octet-stream";
-        out.print("HTTP/1.0 200 OK\r\n"+
-          "Content-type: "+mimeType+"\r\n\r\n");
-
-        // Send file contents to client, then close the connection
-        byte[] a=new byte[4096];
-        int n;
-        while ((n=f.read(a))>0)
-          out.write(a, 0, n);
-        out.close();
-      }
-      catch (FileNotFoundException x) {
-        out.println("HTTP/1.0 404 Not Found\r\n"+
-          "Content-type: text/html\r\n\r\n"+
-          "<html><head></head><body>"+filename+" not found</body></html>\n");
-        out.close();
-      }
+        System.err.println( "Done handling connection." );
+        return;
     }
-    catch (IOException x) {
-      System.out.println(x);
+
+    /**
+    * Read the HTTP request header.
+    **/
+    private String readHTTPRequest( InputStream is )
+    {
+        String line;
+        BufferedReader r = new BufferedReader( new InputStreamReader( is ) );
+
+        String urlAddress = "";
+
+        while ( true ) {
+            try {
+                while ( !r.ready() ) Thread.sleep( 1 );
+                line = r.readLine();
+                if ( line.contains( "GET " ) ) {
+                    urlAddress = line.substring( 4 );
+                    for( int i = 0 ; i < urlAddress.length() ; i++ ) {
+                        if ( urlAddress.charAt( i ) == ' ' )
+                            urlAddress = urlAddress.substring(0 , i );
+                    }//end for
+                }//end if
+                System.err.println( "Request line: (" + line + ")" );
+                if ( line.length() == 0 )
+                    break;
+            } catch ( Exception e ) {
+                System.err.println( "Request error: " + e );
+                break;
+            }
+        }
+        return urlAddress;
     }
-  }
-}
+
+    /**
+    * Write the HTTP header lines to the client network connection.
+    * @param os is the OutputStream object to write to
+    * @param contentType is the string MIME content type (e.g. "text/html")
+    **/
+    private void writeHTTPHeader( OutputStream os , String contentType , String urlAddress ) throws Exception
+    {
+        Date d = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        df.setTimeZone( TimeZone.getTimeZone("GMT") );
+        //if the file does not exist change the HTTP status
+        try {
+            FileReader file = new FileReader( urlAddress );
+            BufferedReader r = new BufferedReader( file );
+        } catch( FileNotFoundException e ) {
+            System.out.println( "File not found: " + urlAddress );
+            os.write( "HTTP/1.1 404 Not Found\n".getBytes() );
+        }
+        //otherwise keep the status 200 OK
+        os.write( "HTTP/1.1 200 OK\n".getBytes() );
+        os.write( "Date: ".getBytes());
+        os.write( ( df.format( d ) ).getBytes() );
+        os.write( "\n".getBytes() );
+        os.write( "Server: Adrian's very own server\n".getBytes() );
+        //os.write( "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes() );
+        //os.write( "Content-Length: 438\n".getBytes() );
+        os.write( "Connection: close\n".getBytes() );
+        os.write( "Content-Type: ".getBytes() );
+        os.write( contentType.getBytes() );
+        os.write( "\n\n".getBytes() ); // HTTP header ends with 2 newlines
+        return;
+    }
+
+    /**
+    * Write the data content to the client network connection. This MUST
+    * be done after the HTTP header has been written out.
+    * @param os is the OutputStream object to write to
+    **/
+    private void writeContent(OutputStream os , String contentType , String urlAddress) throws Exception
+    {
+        Date d = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        df.setTimeZone(TimeZone.getTimeZone( "GMT" ));
+        String dataContent = "";
+        String urlAddressCopy = "." + urlAddress.substring( 0 , urlAddress.length( ) );
+        String date = df.format( d );
+        File in = new File( urlAddressCopy );
+
+        // handel plain html text
+        if ( contentType.equals( "text/html" ) ) {
+            try{
+                FileReader inRead = new FileReader( in );
+                BufferedReader inBuffer = new BufferedReader( inRead );
+                while( ( dataContent = inBuffer.readLine() ) != null ) {
+                    os.write( dataContent.getBytes() );
+                    os.write( "\n".getBytes());
+                    if ( dataContent.contains( "<cs371date>" ) ) {
+                        os.write( date.getBytes() );
+                    }
+                    if ( dataContent.contains( "<cs371server>" ) )
+                        os.write( "My Server's ID string\n".getBytes() );
+                }
+                
+            } catch(FileNotFoundException e) {
+                System.err.println( "File not found: " + urlAddress );
+                os.write( "<h1>Error: 404 Not found<h1>\n".getBytes() );
+            }
+        }
+        // handle image files
+        else if ( contentType.contains( "image" ) ) {
+
+        FileInputStream imageReader = new FileInputStream( in );
+        byte imageArray[] = new byte [ (int) in.length() ];
+        imageReader.read( imageArray );
+        DataOutputStream imageOut = new DataOutputStream( os );
+        imageOut.write( imageArray );
+
+        }
+    }
+
+} // end class
