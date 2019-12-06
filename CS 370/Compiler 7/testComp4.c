9e4b1a82b8c0f6c8b6358a8027c2e3be2566ab52
Joseph Camacho <camismacho@yahoo.com>
2019-12-06 13:52:35 -0700

    comp 7 progress

--- CS 370/Compiler 7/testComp4.c
+++ CS 370/Compiler 7/testComp4.c
@@ -39,12 +39,6 @@ main(int argc, char* argv)
        x = x + 1;
     }
    
-   y = 0;
-   do {
-       puts("do while loop m8\n");
-       y = y + 1;
-   } while (y < 10);
-
 }
 
 test (int one, int two, int three, int four)
---
+++ CS 370/Compiler 7/testComp7.c
@@ -0,0 +1,10 @@
+int y;
+main(int argc, char* argv)
+{
+   y = 0;
+   do {
+       puts("do while loop m8\n");
+       y = y + 1;
+   } while (y < 10);
+
+}
