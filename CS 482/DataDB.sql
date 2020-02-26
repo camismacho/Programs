934d0bc7299fbe1df0f7a0bd36624edd8dadc445
Joseph Camacho <camismacho@yahoo.com>
2020-02-25 18:52:10 -0700

    repo refresh

--- CS 482/DataDB.sql
+++ CS 482/DataDB.sql
@@ -1,11 +1,11 @@
 /*Author: Joseph Camacho-Terrazas*/
 /*DataDB.sql*/
 
-insert into Players values('Pistol Pete',1001,'Aggies','QB',10,4500,100000);
-insert into Players values('John Doe',1002,'Cowboys','RB',30,500,2000);
-insert into Players values('Tony Stark',1003,'Dodgers','WR',2,26,30000);
-insert into Players values('John Price',1004,'Lakers','QB',0,13,1500000);
-insert into Players values('Dr. Phil',1005,'Professionals','RB',10004,567000,65000);
+insert into Players values('Zack Martin',1001,'Cowboys','QB',10,4500,100000);
+insert into Players values('Tyron Smith',1002,'Cowboys','RB',30,500,2000);
+insert into Players values('Olivier Vernon',1003,'Browns','WR',2,26,30000);
+insert into Players values('Tony Gonzalez',1004,'Chiefs','QB',0,13,1500000);
+insert into Players values('Andrew Whitworth',1005,'Rams','RB',14,5670,65000);
 
 insert into Games values(501,'1995-05-13','Aggie Memorial','W',15000,2000000);
 insert into Games values(502,'1996-08-19','Taco Bell Arena','L',10000,6000000);
@@ -17,4 +17,4 @@ insert into Play values(1001, 501);
 insert into Play values(1002, 502);
 insert into Play values(1003, 503);
 insert into Play values(1004, 504);
-insert into Play values(1005, 505);00
+insert into Play values(1005, 505);
