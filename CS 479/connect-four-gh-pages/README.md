c1b2410a1729cf932e447f16a1c8aeccf3b71637
Joseph <camismacho@yahoo.com>
2020-02-26 13:08:08 -0700

    Starting js game

---
+++ CS 479/connect-four-gh-pages/README.md
@@ -0,0 +1,6 @@
+# Connect Four
+
+An implementation of Connect Four in Javascript.
+
+**Play: [http://bryanbraun.github.io/connect-four](http://bryanbraun.github.io/connect-four)**
+
---
+++ CS 479/connect-four-gh-pages/js/vars.js
@@ -0,0 +1,25 @@
+// Create variables for use in our game.
+var Game = {};
+
+// Global game config
+Game.config = {
+  startingPlayer: "black", // Choose 'black' or 'red'.
+  takenMsg: "You cannot place a piece here. Please choose another location.",
+  drawMsg: "Game Draw! Try Again?",
+  winMsg: "Winner Winner Chicken Dinner! Congrats ",
+  countToWin: 4,
+
+  // note: board dimensions are zero-indexed
+  boardLength: 6,
+  boardHeight: 5,
+};
+
+// Global Game State
+Game.board = [[0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0]];
+
+Game.currentPlayer = Game.config.startingPlayer;
