5c726dd67fa5fcd63b948d5e4f85c070875f9864
Joseph Camacho <camismacho@yahoo.com>
2020-02-26 13:08:34 -0700

    repo refresh

--- CS 482/DataDB.sql
+++ CS 482/DataDB.sql
@@ -1,17 +1,17 @@
 /*Author: Joseph Camacho-Terrazas*/
 /*DataDB.sql*/
 
-insert into Players values('Zack Martin',1001,'Cowboys','QB',10,4500,100000);
-insert into Players values('Tyron Smith',1002,'Cowboys','RB',30,500,2000);
-insert into Players values('Olivier Vernon',1003,'Browns','WR',2,26,30000);
-insert into Players values('Tony Gonzalez',1004,'Chiefs','QB',0,13,1500000);
-insert into Players values('Andrew Whitworth',1005,'Rams','RB',14,5670,65000);
+insert into Players values('Jared Goff',1001,'Rams','QB',22,4638,600000);
+insert into Players values('Leonard Fournette',1002,'Jaguars','RB',30,500,200000);
+insert into Players values('Julio Jones',1003,'Falcons','WR',6,1394,300000);
+insert into Players values('Matthew Stafford',1004,'Lions','QB',19,2499,1500000);
+insert into Players values('Devin Singletary',1005,'Bills','RB',2,775,100000);
 
-insert into Games values(501,'1995-05-13','Aggie Memorial','W',15000,2000000);
-insert into Games values(502,'1996-08-19','Taco Bell Arena','L',10000,6000000);
-insert into Games values(503,'1998-10-16','Barclays Center','T',30000,9000000);
-insert into Games values(504,'1995-03-01','Metlife Stadium','L',5000,1000000);
-insert into Games values(505,'1997-09-25','FedEx Field','L',19000,5000000);
+insert into Games values(501,'2008-05-13','Levis Stadium','W',15000,2000000);
+insert into Games values(502,'2010-08-19','Gilette Stadium','L',10000,6000000);
+insert into Games values(503,'2015-10-16','Barclays Center','T',30000,9000000);
+insert into Games values(504,'2013-03-01','Metlife Stadium','L',5000,1000000);
+insert into Games values(505,'2014-09-25','FedEx Field','L',19000,5000000);
 
 insert into Play values(1001, 501);
 insert into Play values(1002, 502);
